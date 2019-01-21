d5884285c502efaa439ffe79064912569b9caf40
cagy <ml.cagy@seznam.cz>
2019-01-21 20:25:44 +0100

    Initial Commit

---
+++ NTFS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NTFS
+//
+//  Created by Jan Čarnogurský on 21/01/2019.
+//  Copyright © 2019 Jan Čarnogurský. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
